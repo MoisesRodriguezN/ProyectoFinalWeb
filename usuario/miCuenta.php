5fdedd7cff2135cb4b9110f25170e58a79d008ea
MoisesRodriguezN <moises_rodiguez@hotmail.com>
2016-12-01 00:29:15 +0100

    Mejoras

--- usuario/miCuenta.php
+++ usuario/miCuenta.php
@@ -72,6 +72,7 @@
     </head>
     <body class="fondoCuerpo">
       <?php
+      if ( $_SESSION['logueadoUser'] == true){
       try {
           $conexion = new PDO("mysql:host=localhost;dbname=hotel;charset=utf8", "root");
       } catch (PDOException $e) {
@@ -186,6 +187,9 @@
               </div>
               <?php
             }
+      }else{
+        header("location:login.php");
+      }
             ?>
         </div>
     </body>
