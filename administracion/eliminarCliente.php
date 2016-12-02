08d16c75363ac199b12f07c1730c66387c6168e1
MoisesRodriguezN <moises_rodiguez@hotmail.com>
2016-12-02 11:25:20 +0100

    Actualizando CSS a responsive

--- administracion/eliminarCliente.php
+++ administracion/eliminarCliente.php
@@ -6,7 +6,8 @@
     <head>
         <meta charset="UTF-8">
         <title>Eliminar cliente</title>
-        <link rel="stylesheet" type="text/css" href="../css/style.css">
+        <link rel="stylesheet" type="text/css" href="../css/Cuerpo.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
     <body>
         <?php
