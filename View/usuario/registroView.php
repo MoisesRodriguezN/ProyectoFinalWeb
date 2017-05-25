8ff8886a821118d829d748400bad8f63ab186fc1
Moisés <moises_rodiguez@hotmail.com>
2017-05-25 18:03:29 +0200

    Mejoras. Ahora se pide por segunda vez la clave para coprobar que son iguales.

--- View/usuario/registroView.php
+++ View/usuario/registroView.php
@@ -6,63 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
         <script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.15.0/jquery.validate.js"></script>
-        <script src="../../View/js/usuario/registroView.js"
-        <script>
-//            $(document).ready(function(){
-//                $("#claveUser").focusin(function(){
-//                    $("#nombreUser").fadeOut(300, function(){
-//                        $("#claveUserComprueba").fadeIn(200);
-//                    });    
-//                });
-//                
-//                $(".camposRegistroUsusarios").dblclick(function(){
-//                    $("#claveUserComprueba").fadeOut(300, function(){
-//                        $("#nombreUser").fadeIn(200);
-//                    });    
-//                });
-//                                        
-//                
-//                $("#formRegistroUsusarios").validate({
-//                  rules: {
-//                    clave: {required: true, minlength: 6, maxlength: 20},
-//                      claveComprueba: {
-//                        equalTo: "#claveUser"
-//                      },
-//                      
-//                      dni: {required: true, minlength: 9, maxlength: 9},
-//                      
-//                      nombre: {required: true},
-//                      apellido1: {required: true},
-//                      apellido2: {required: true},
-//                      usuario: {required: true}
-//
-//
-//                  },
-//                  messages: {
-//                      clave: "Clave de 6 carácteres mínimo",
-//                      dni: "Introduce un DNI válido",
-//                       nombre: "Introduce un nombre",
-//                      apellido1: "El campo está requerido",
-//                      apellido2: "El campo está requerido",
-//                      usuario: "El campo está requerido",
-//                      claveComprueba: "Introduce la misma clave"
-//                  },
-//                  
-//                  errorPlacement: function(error, element) {
-//                        error.insertBefore(element);
-//                    }
-//                });
-//            });
-            
-            
-
-        </script>
-        
-        <style>
-            #claveUserComprueba{
-                display: none;
-            }
-        </style>
+        <script src="../../View/js/usuario/registroView.js"></script>
         <meta charset="UTF-8">
         <title>Iniciar sesión</title>
     </head>
