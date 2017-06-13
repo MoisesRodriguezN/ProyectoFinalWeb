db30486c113f3b6ca49f8f5bc5834491810a82c6
Moisés <moises_rodiguez@hotmail.com>
2017-06-13 15:42:12 +0200

    Mejoras

--- Controller/administracion/altaCliente.php
+++ Controller/administracion/altaCliente.php
@@ -3,11 +3,19 @@ session_start();
 if ($_SESSION['logueadoAdmin'] == true) {
 require_once 'compruebaDB.php';
 require_once '../../Model/Cliente.php';
+require_once '../../Model/Login.php';
 
-$clienteAux = new Cliente("", $_POST[dni], $_POST[nombre], 
+$dni = $_POST[dni];
+
+$clienteAux = new Cliente("", $dni, $_POST[nombre], 
         $_POST[apellido1], $_POST[apellido2]);
 
 $clienteAux->addCliente();
+
+
+$data['clientes'] = Cliente::getClienteByDniNoPag($dni);
+
+Login::creaUser($_POST['usuario'], $_POST['clave'], $data['clientes'][0]->getCodCliente());
 
 require_once './obtieneListaClientes.php';
 }else {
