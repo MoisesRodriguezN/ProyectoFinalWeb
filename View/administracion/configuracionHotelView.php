07c65b231637c4df641bc16d1ee56ec7a5c557f6
Moisés <moises_rodiguez@hotmail.com>
2017-06-13 13:32:27 +0200

    Mejoras

--- View/administracion/configuracionHotelView.php
+++ View/administracion/configuracionHotelView.php
@@ -99,7 +99,7 @@ if ($_SESSION['logueadoAdmin']) {
                                 estado: estado
                             }, function (data, status) {
                                 var response = $('<html />').html(data);
-    //                                    response.find('#pag2Texto2 P')
+                                //                                    response.find('#pag2Texto2 P')
                                 $(".estados").html(response.find('.estados'));
                             });
                         }
@@ -134,8 +134,8 @@ if ($_SESSION['logueadoAdmin']) {
                         modal: true,
                         width: 'auto'
                     });
-                    
-                     $("#dialogoCorrecto3").dialog({
+
+                    $("#dialogoCorrecto3").dialog({
                         autoOpen: false,
                         resizable: false,
                         modal: true,
@@ -196,19 +196,19 @@ if ($_SESSION['logueadoAdmin']) {
                         modal: true,
                         buttons: {
                             "Guardar": function () {
-                                    $.post("actualizaNombreHotel.php", {
-                                        nombre: $("#inputNombre").val()
-                                    }, function (data, status) {
-                                        var response = $('<html />').html(data);
-                                        $("#nombreHotel").html(response.find('#nuevoNombre'));
-                                        $("#dialogoCorrecto3").dialog('open');
-                                        setTimeout(function () {
-                                            $("#dialogoCorrecto3").dialog("close");
-                                        }, 1500);
-                                    });//get			
+                                $.post("actualizaNombreHotel.php", {
+                                    nombre: $("#inputNombre").val()
+                                }, function (data, status) {
+                                    var response = $('<html />').html(data);
+                                    $("#nombreHotel").html(response.find('#nuevoNombre'));
+                                    $("#dialogoCorrecto3").dialog('open');
+                                    setTimeout(function () {
+                                        $("#dialogoCorrecto3").dialog("close");
+                                    }, 1500);
+                                });//get			
 
-                                    $(this).dialog("close");
- 
+                                $(this).dialog("close");
+
                             },
                             "Cancelar": function () {
                                 $(this).dialog("close");
@@ -222,32 +222,32 @@ if ($_SESSION['logueadoAdmin']) {
                         $("#inputNombre").val($("#nombreHotel").text());
                     });
 
-                      
-                       $("#dialogoNuevaUrlSocial").dialog({
+
+                    $("#dialogoNuevaUrlSocial").dialog({
                         autoOpen: false,
                         resizable: false,
                         minWidth: 200,
                         modal: true,
                         buttons: {
                             "Guardar": function () {
-                                    $.post("actualizaUrlSocial.php", {
-                                        url: $("#inputUrl").val(),
-                                        id: $('select[name=redesSociales]').val()
-                                    }, function (data, status) {
-                                        $("#dialogoCorrecto3").dialog('open');
-                                        setTimeout(function () {
-                                            $("#dialogoCorrecto3").dialog("close");
-                                        }, 1500);
-                                    });			
+                                $.post("actualizaUrlSocial.php", {
+                                    url: $("#inputUrl").val(),
+                                    id: $('select[name=redesSociales]').val()
+                                }, function (data, status) {
+                                    $("#dialogoCorrecto3").dialog('open');
+                                    setTimeout(function () {
+                                        $("#dialogoCorrecto3").dialog("close");
+                                    }, 1500);
+                                });
 
-                                    $(this).dialog("close");
+                                $(this).dialog("close");
                             },
                             "Cancelar": function () {
                                 $(this).dialog("close");
                             }
                         }
                     });
-                    
+
                     //Boton Cambiar Nombre	
                     $(document).on("click", "#editSocial", function () {
                         $("#dialogoNuevaUrlSocial").dialog("open");
@@ -274,83 +274,83 @@ if ($_SESSION['logueadoAdmin']) {
         </head>
         <body>
             <?php
-            //if ($_SESSION['logueadoAdmin']){
-            ?>
-            <div class="container">
-                <nav class="navbar navbar-inverse">
-                    <div class="container-fluid">
-                        <div class="navbar-header">
-                            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                                <span class="icon-bar"></span>
-                                <span class="icon-bar"></span>
-                                <span class="icon-bar"></span>
-                            </button>
-                            <a class="navbar-brand" href="index.php">Administración Hotel</a>
+            if ($_SESSION['logueadoAdmin']) {
+                ?>
+                <div class="container">
+                    <nav class="navbar navbar-inverse">
+                        <div class="container-fluid">
+                            <div class="navbar-header">
+                                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                </button>
+                                <a class="navbar-brand" href="index.php">Administración Hotel</a>
+                            </div>
+                            <div class="collapse navbar-collapse" id="myNavbar">
+                                <ul class="nav navbar-nav">
+                                    <li><a href="index.php">Clientes</a></li>
+                                    <li><a href="../../Controller/administracion/habitaciones.php">Habitaciones</a></li>
+                                    <li><a href="reservas.php">Reservas</a></li>
+                                    <li class="active"><a href="../../Controller/administracion/configuracionHotel.php">Configuración</a></li>
+                                    <!--<li><a href="#">Page 3</a></li>-->
+                                </ul>
+                                <ul class="nav navbar-nav navbar-right">
+                                    <li><a href="index.php"><span class="glyphicon glyphicon-user"></span> <?= ucfirst($_SESSION['nombreAdmin']) ?></a></li> 
+                                    <li><a href="logout.php"><span class="glyphicon glyphicon-log-out"></span> Cerrar sesión</a></li> 
+                                </ul> <!-- ucfirst() Pone en mayúscula la primera letra-->
+                            </div>
                         </div>
-                        <div class="collapse navbar-collapse" id="myNavbar">
-                            <ul class="nav navbar-nav">
-                                <li><a href="index.php">Clientes</a></li>
-                                <li><a href="../../Controller/administracion/habitaciones.php">Habitaciones</a></li>
-                                <li><a href="reservas.php">Reservas</a></li>
-                                <li class="active"><a href="../../Controller/administracion/configuracionHotel.php">Configuración</a></li>
-                                <!--<li><a href="#">Page 3</a></li>-->
-                            </ul>
-                            <ul class="nav navbar-nav navbar-right">
-                                <li><a href="index.php"><span class="glyphicon glyphicon-user"></span> <?= ucfirst($_SESSION['nombreAdmin']) ?></a></li> 
-                                <li><a href="logout.php"><span class="glyphicon glyphicon-log-out"></span> Cerrar sesión</a></li> 
-                            </ul> <!-- ucfirst() Pone en mayúscula la primera letra-->
-                        </div>
-                    </div>
-                </nav>
+                    </nav>
 
-                <h2>Configuración del Hotel</h2>
-                <p>Aquí puedes cambiar las cofiguraciones del hotel, como el nombre, la clave de administrador, la galería de imagenes, logos...</p>            
-                <table class="table table-striped">
-                    <thead>
-                        <tr class="info">
-                            <th>Elemento</th>
-                            <th>Valor</th>
-                            <th>Editar</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>Nombre del hotel
-                            </td>
-                            <td id="nombreHotel"><?= $nombreHotel->nombreHotel ?></td>
-                            <td><button type="button" class="btn btn-info" id="editNombreHotel">Editar</button></td>
-                        </tr>
-                        <tr>
-                            <td>Contraseña administrador
-                            </td>
-                            <td>**************</td>
-                            <td><button type="button" class="btn btn-info" id="cambiarClave">Editar</button></td>
-                        </tr>
-                        <tr>
-                        <tr>
-                            <td>Documentación
-                            </td>
-                            <td>Español</td>
-                            <td><button type="button" class="btn btn-info" id="cambiarClave">Ver</button></td>
-                        </tr>
-                        <tr>
-                            <td>Documentación funcionalidades
-                            </td>
-                            <td>Ingles</td>
-                            <td><button type="button" class="btn btn-info" id="cambiarClave">Ver</button></td>
-                        </tr>
-                            <td>Enlaces Redes Sociales
-                            </td>
-                            <td>
-                                <select name="redesSociales" id="redesSocialesSelect">
-                                    <option value="facebook">Facebook</option>
-                                    <option value="googlePlus">Google Plus</option>
-                                    <option value="instagram">Instagram</option>
-                                    <option value="twitter">Twitter</option>
-                                </select>
-                                <input type="checkbox" name="estadoCheck" id="estadoCheck" value="estadoHab">Habilitar/Deshabilitar
-                            </td>
-                            <td><button type="button" class="btn btn-info" id="editSocial">Editar</button></td>
+                    <h2>Configuración del Hotel</h2>
+                    <p>Aquí puedes cambiar las cofiguraciones del hotel, como el nombre, la clave de administrador, la galería de imagenes, logos...</p>            
+                    <table class="table table-striped">
+                        <thead>
+                            <tr class="info">
+                                <th>Elemento</th>
+                                <th>Valor</th>
+                                <th>Editar</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Nombre del hotel
+                                </td>
+                                <td id="nombreHotel"><?= $nombreHotel->nombreHotel ?></td>
+                                <td><button type="button" class="btn btn-info" id="editNombreHotel">Editar</button></td>
+                            </tr>
+                            <tr>
+                                <td>Contraseña administrador
+                                </td>
+                                <td>**************</td>
+                                <td><button type="button" class="btn btn-info" id="cambiarClave">Editar</button></td>
+                            </tr>
+                            <tr>
+                            <tr>
+                                <td>Documentación
+                                </td>
+                                <td>Español</td>
+                                <td><button type="button" class="btn btn-info" id="cambiarClave">Ver</button></td>
+                            </tr>
+                            <tr>
+                                <td>Documentación funcionalidades
+                                </td>
+                                <td>Ingles</td>
+                                <td><button type="button" class="btn btn-info" id="cambiarClave">Ver</button></td>
+                            </tr>
+                        <td>Enlaces Redes Sociales
+                        </td>
+                        <td>
+                            <select name="redesSociales" id="redesSocialesSelect">
+                                <option value="facebook">Facebook</option>
+                                <option value="googlePlus">Google Plus</option>
+                                <option value="instagram">Instagram</option>
+                                <option value="twitter">Twitter</option>
+                            </select>
+                            <input type="checkbox" name="estadoCheck" id="estadoCheck" value="estadoHab">Habilitar/Deshabilitar
+                        </td>
+                        <td><button type="button" class="btn btn-info" id="editSocial">Editar</button></td>
                         </tr>
                         <tr>
                             <td>Logo Hotel</td>
@@ -486,74 +486,77 @@ if ($_SESSION['logueadoAdmin']) {
 
                             </form>
                         </tr>
-                    </tbody>
-                </table>
-            </div>
+                        </tbody>
+                    </table>
+                </div>
 
-            <div id="dialogoCargando" style="position: relative;"title="Procesando contenido">
-                <p>El contenido está siendo procesado.</p>
+                <div id="dialogoCargando" style="position: relative;"title="Procesando contenido">
+                    <p>El contenido está siendo procesado.</p>
 
-                <p>Puede tardar unos segundos</p>
-            </div>
+                    <p>Puede tardar unos segundos</p>
+                </div>
 
-            <div id="dialogoCorrecto" title="Clave Actualizada">
-                <p>
-                    La clave ha sido guardada correctamente.
-                </p>
+                <div id="dialogoCorrecto" title="Clave Actualizada">
+                    <p>
+                        La clave ha sido guardada correctamente.
+                    </p>
 
-                <P>
-                    Vuelve a iniciar sesión con la nueva clave.
-                </P>
-            </div>
+                    <P>
+                        Vuelve a iniciar sesión con la nueva clave.
+                    </P>
+                </div>
 
-            <div id="dialogoCorrecto2" title="Nombre guardado">
-                <p>
-                    El nombre ha sido guardado
-                </p>
-            </div>
-            
-            <div id="dialogoCorrecto3" title="Url Guardada">
-                <p>
-                    La url ha sido guardada
-                </p>
-            </div>
+                <div id="dialogoCorrecto2" title="Nombre guardado">
+                    <p>
+                        El nombre ha sido guardado
+                    </p>
+                </div>
 
-            <div id="dialogoNuevaClave" title="Modificación clave de admin">
-                <?php include "../../View/administracion/formModificarClaveAdmin.php" ?>
-            </div>
+                <div id="dialogoCorrecto3" title="Url Guardada">
+                    <p>
+                        La url ha sido guardada
+                    </p>
+                </div>
 
-            <div id="dialogoNuevoNombre" title="Modificación nombre Hotel">
-                <?php include "../../View/administracion/formModificarNombreHotel.php" ?>
-            </div>
-            
-            <div id="dialogoNuevaUrlSocial" title="Modificación URL Botón Social">
-                <?php include "../../View/administracion/formModificarURLRedSocial.php" ?>
-            </div>
+                <div id="dialogoNuevaClave" title="Modificación clave de admin">
+                    <?php include "../../View/administracion/formModificarClaveAdmin.php" ?>
+                </div>
 
-            <div class="estados elementosOcultos">
-                <?php
-                foreach ($estadoImg as $key => $value) {
-                    ?>
-                    <div id="<?= $key ?>"><?= $value->estado ?></div>    
+                <div id="dialogoNuevoNombre" title="Modificación nombre Hotel">
+                    <?php include "../../View/administracion/formModificarNombreHotel.php" ?>
+                </div>
+
+                <div id="dialogoNuevaUrlSocial" title="Modificación URL Botón Social">
+                    <?php include "../../View/administracion/formModificarURLRedSocial.php" ?>
+                </div>
+
+                <div class="estados elementosOcultos">
                     <?php
-                }
-                ?>
+                    foreach ($estadoImg as $key => $value) {
+                        ?>
+                        <div id="<?= $key ?>"><?= $value->estado ?></div>    
+                        <?php
+                    }
+                    ?>
 
-            </div>
+                </div>
 
-            <div id="nombreAdmin" class="elementosOcultos">
-    <?= $_SESSION['nombreAdmin']; ?>
-            </div>
-        </body>
-    </html>
+                <div id="nombreAdmin" class="elementosOcultos">
+                    <?= $_SESSION['nombreAdmin']; ?>
+                </div>
+            </body>
+        </html>
 
-    <?php
-} else {
-    //Error, mensaje, redirección...
-    echo "Zona Inaccesible. Requiere Inicio de sesión"; //Mensaje de prueba
-    ?>
-    <script>
-        window.location.href = "../../Controller/administracion/login.php";
-    </script>
-    <?php
+        <?php
+    } else {
+        //Error, mensaje, redirección...
+        echo "Zona Inaccesible. Requiere Inicio de sesión"; //Mensaje de prueba
+        ?>
+        <script>
+            window.location.href = "../../Controller/administracion/login.php";
+        </script>
+        <?php
+    }
+}else{
+    header("location:../../Controller/administracion/login.php");
 }
